b7562477deed0bd06bf027d7c52dc376b4303673
Samuel Lazareanu <52662032+SamuraiPolix@users.noreply.github.com>
2023-06-25 15:27:21 +0300

    update

--- main.py
+++ main.py
@@ -5,32 +5,18 @@ import verse_handler
 from Fonts import Fonts
 
 # Define the paths and values to everything
-<<<<<<< Updated upstream
-number_of_videos = -1
-=======
 number_of_videos = 2
->>>>>>> Stashed changes
 project_dir = os.getcwd().replace("\\", "/")
 
 video_folder = f"{project_dir}/videos"
 # video_folder = "E:/Bots/VideoMaker/videos/caribbean/darken0.4"
 audio_folder = f"{project_dir}/audio"
-<<<<<<< Updated upstream
-json_file = f"{project_dir}/sources/verses_data/cedoninvestment_data.json"
-fonts_dir = f"{project_dir}/sources/fonts"
-output_folder = f"{project_dir}/customers"
-# text_source_font = f'{project_dir}/sources/MouldyCheeseRegular-WyMWG.ttf'.replace(":/", "\:/")
-text_source_font = f'{project_dir}/sources/fonts/PermanentMarker-Regular.ttf'.replace(":/", "\:/")
-image_file = f"{project_dir}/sources/logo3.png"
-customer_name = "cedoninvestment"
-=======
-json_file = f"{project_dir}/sources/verses_data/love_data.json"
+json_file = f"{project_dir}/sources/verses_data/nvenlightenment_data.json"
 fonts_dir = f"{project_dir}/sources/fonts"
 output_folder = f"{project_dir}/customers"
 text_source_font = f'{project_dir}/sources/MouldyCheeseRegular-WyMWG.ttf'.replace(":/", "\:/")
-image_file = f"{project_dir}/sources/nvenlightenment.png"
-customer_name = "nvenlightenment_finaltest2"
->>>>>>> Stashed changes
+image_file = f"{project_dir}/sources/nvenlightenment4.png"
+customer_name = "nvenlightenment"
 verse_text_image_path = f"{project_dir}/verse_images/{customer_name}"
 fonts_paths = [f'{project_dir}/sources/fonts/CoffeeJellyUmai.ttf', f'{project_dir}/sources/fonts/CourierprimecodeRegular.ttf', f'{project_dir}/sources/fonts/EbGaramond08Regular-2mWe.ttf', f'{project_dir}/sources/fonts/FlowersSunday.otf', f'{project_dir}/sources/fonts/GreenTeaJelly.ttf', f'{project_dir}/sources/fonts/HeyMarch.ttf', f'{project_dir}/sources/fonts/Hugamour.ttf', f'{project_dir}/sources/fonts/LetsCoffee.otf', f'{project_dir}/sources/fonts/Lightning Script.ttf', f'{project_dir}/sources/fonts/LikeSlim.ttf', f'{project_dir}/sources/fonts/PineappleDays.ttf', f'{project_dir}/sources/fonts/SunnySpellsBasicRegular.ttf', f'{project_dir}/sources/fonts/TakeCoffee.ttf', f'{project_dir}/sources/fonts/WantCoffee.ttf']
 fonts_sizes = [95, 70, 70, 65, 85, 75, 73, 50, 85, 75, 52, 87, 50, 65]
